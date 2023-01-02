4d5e6456e01305ec4c3915e91f0d8e5e66b8be6f
yeobi01 <yeobi_01@naver.com>
2023-01-02 23:55:22 +0900

    C++

---
+++ 큰돌_C++/2주차/2F_2828.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int dy[4] = {0, 0, -1, 1};
+int dx[4] = {1, -1, 0, 0};
+
+int N;
+char a[65][65];
+
+string quard(int y, int x, int size){
+    string ret = "";
+    char temp = a[y][x];
+
+    if(size == 1) { ret += temp; return ret; }
+    
+    for(int i = y; i < y + size; i++){
+        for(int j = x; j < x + size; j++){
+            if(temp != a[i][j]){
+                ret += '(';
+                ret += quard(y, x, size/2);
+                ret += quard(y, x + size/2, size/2);
+                ret += quard(y + size/2, x, size/2);
+                ret += quard(y + size/2, x + size/2, size/2);
+                ret += ')';
+                return ret;
+            }
+        }
+    }
+    
+    ret += temp;
+    return ret;
+} 
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N;
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            cin >> a[i][j];
+        }
+    }
+    cout << quard(0, 0, N) << "\n";
+    return 0;
+}
