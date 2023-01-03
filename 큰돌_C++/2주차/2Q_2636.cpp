a3b0653f04d66fe1ff87a3f136e3b28fd266ae7e
yeobi01 <yeobi_01@naver.com>
2023-01-03 21:04:55 +0900

    C++

--- 큰돌_C++/2주차/2Q_2636.cpp
+++ 큰돌_C++/2주차/2Q_2636.cpp
@@ -4,10 +4,57 @@ using namespace std;
 int dy[4] = {-1, 1, 0, 0};
 int dx[4] = {0, 0, -1, 1};
 
+int N, M, a[101][101], visited[101][101], y, x, t, ret;
+vector<pair<int,int>> v;
+
+void DFS(int y, int x){ 
+    visited[y][x] = 1; 
+    if(a[y][x] == 1) { 
+        v.push_back({y, x}); 
+        return; 
+    } 
+    for(int i = 0; i < 4; i++){ 
+        int ny = y + dy[i]; 
+        int nx = x + dx[i]; 
+        if(ny < 0 || ny >= N || nx < 0 || nx >= M || visited[ny][nx]) continue; 
+        DFS(ny, nx); 
+    } 
+    return; 
+}
+
+bool check(){
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < M; j++){
+            if(a[i][j] == 1) return true;
+        }
+    }
+    return false;
+}
+
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
+    cin >> N >> M;
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < M; j++){
+            cin >> a[i][j];
+        }
+    }
+
+    while(true){
+        ret = 0;
+        fill(&visited[0][0], &visited[0][0] + 101*101, 0); v.clear();
+        DFS(0, 0);
+        for(auto x : v){
+            ret++;
+            a[x.first][x.second] = 0;
+        }
+        t++;
+        if(!check()) break;
+    }
+    
+    cout << t << "\n" << ret << "\n";
     return 0;
 }
---
+++ 큰돌_C++/2주차/2R_1068.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int dy[4] = {-1, 1, 0, 0};
+int dx[4] = {0, 0, -1, 1};
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    return 0;
+}
