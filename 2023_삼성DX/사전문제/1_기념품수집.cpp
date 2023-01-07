a8c35638db94067b8f2f4ce4205a768f66eab612
yeobi01 <yeobi_01@naver.com>
2023-01-07 20:49:46 +0900

    C++

--- 2023_삼성DX/사전문제/1_기념품수집.cpp
+++ 2023_삼성DX/사전문제/1_기념품수집.cpp
@@ -1,37 +1,66 @@
 #include <bits/stdc++.h>
 using namespace std;
+const int dy[4] = {0, 0, 1, -1};
+const int dx[4] = {1, -1, 0, 0};
+
+int T, R, C, ret, visited[27];
+char arr[21][21];
+
+void DFS(int y, int x, int cnt){
+    ret = max(ret, cnt);
+
+    for(int i = 0; i < 4; i++){
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+        if(ny < 0 || ny >= R || nx < 0 || nx >= C || visited[arr[ny][nx] - 'A']) continue;
+        visited[arr[ny][nx] - 'A'] = 1;
+        DFS(ny, nx, cnt + 1);
+        visited[arr[ny][nx] - 'A'] = 0;
+    }
+
+    return;
+}
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int N;
-    while(N--){
-        string s, ret = "";
-        char low, high;
-        cin >> s >> low >> high;
+    cin >> T;
+    for(int t = 1; t <= T; t++){
+        fill(&visited[0], &visited[0] + 26, 0); ret = 0;
 
-        bool flag = false;
-        for(int i = 0; i < s.length(); i++){
-            if(flag){
-                ret += (high + '0');
-                continue;
-            }
-
-            if(s[i] >= high){
-                ret += (high + '0');
-            } else if(high > s[i] && s[i] > low){
-                ret += (low + '0');
-                flag = true;
-            } else if(low == s[i]){
-                ret += (low + '0');
-            } else if(low > s[i] && s[i] >= 0){
-                ret += '0';
+        cin >> R >> C;
+        for(int i = 0; i < R; i++){
+            for(int j = 0; j < C; j++){
+                cin >> arr[i][j];
             }
         }
 
-        cout << ret << "\n";
-    }
+        visited[arr[0][0] - 'A'] = 1;
+        DFS(0, 0, 1);
 
+        cout << "#" << t << " " << ret << "\n";
+    }
 }
+/*
+3
+2 4
+CAAB
+ADCB
+3 6
+HFDFFB
+AJHGDH
+DGAGEH
+5 5
+IEFCJ
+FHFKC
+FFALF
+HFGCF
+HMCHH
+
+1
+2 4
+CAAB
+ADCB
+*/
--- 2023_삼성DX/사전문제/2_현석이의생일.cpp
+++ 2023_삼성DX/사전문제/2_현석이의생일.cpp
@@ -1,13 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string eraseZero(string s){
-    int i;
-    for(i = 0; i < s.length(); i++){
-        if(s[i] != '0') break;
+int T;
+char low, high;
+string s, ret;
+
+bool check(){
+    for(int i = 0; i < s.length(); i++){
+        if(s[i] > low) break;
+        else if(s[i] == low) continue;
+        else if(s[i] < low) return false;
     }
-    string ret = s.substr(i, s.length() - i);
-    return ret;
+    return true;
 }
 
 int main(){
@@ -15,13 +19,21 @@ int main(){
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int T;
+
     cin >> T;
     for(int t = 1; t <= T; t++){
-        string s, ret = "";
-        char low, high;
         cin >> s;
         cin >> low >> high;
+        ret = "";
+
+        if(!check()){
+            for(int j = 0; j < s.length() - 1; j++){
+                ret += high;
+            }
+            if(ret.size()) cout << "#" << t << " " << ret << "\n";
+            else cout << "#" << t << " -1\n";
+            continue;
+        }
 
         bool flag = false;
         for(int i = 0; i < s.length(); i++){
@@ -40,47 +52,36 @@ int main(){
             } else if(s[i] == low){
                 ret += low;
             } else if(low > s[i]){
-                ret = "";
-                bool flag2 = false;
-                for(int j = 0; j < s.length(); j++){
-                    if(flag2){
-                        ret += high;
-                        continue;
-                    }
-
-                    if(s[j] == high){
-                        ret += low;
-                        flag2 = true;
-                    } else if(s[j] == low){
-                        ret += low;
+                int j;
+                for(j = i - 1; j >= 0; j--){
+                    if(ret[j] == high){
+                        ret[j] = low;
+                        break;
+                    } else{
+                        ret[j] = high;
                     }
                 }
-                if(!flag2){
-                    ret = "";
-                    for(int j = 0; j < s.length() - 1; j++){
-                        ret += high;
-                    }
-                }
-                break;
+                ret += high;
+                flag = true;
             }
         }
 
-        ret = eraseZero(ret);
+        if(ret[0] == '0') ret = ret.substr(1, ret.size() - 1);
+
         if(ret.length()) cout << "#" << t << " " << ret << "\n";
         else cout << "#" << t << " -1\n";
     }
-
     return 0;
 }
 /*
-13
+14
 1 0 1
 12223 1 3
 1101 0 1
 3333 0 3
 3333 0 4
 88880 7 8
-88808 7 8
+78808 7 8
 77770 7 8
 12202 1 2
 123456789 1 9
@@ -88,14 +89,4 @@ int main(){
 2 6 9
 5 0 8
 422223324 2 4
-
-3
-88880 7 8
-88808 7 8
-98808 7 8
-
-3 
-88880 7 8
-88808 7 8
-12202 1 2
 */
