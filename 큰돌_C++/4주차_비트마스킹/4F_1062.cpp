9de722a4876a00f807fc6d6f50e0efb0f2a71aab
yeobi01 <yeobi_01@naver.com>
2023-01-08 18:39:27 +0900

    C++

--- 큰돌_C++/4주차_비트마스킹/4F_1062.cpp
+++ 큰돌_C++/4주차_비트마스킹/4F_1062.cpp
@@ -3,6 +3,7 @@ using namespace std;
 const int INF = 987654321;
 
 int N, K, ret, words[51];
+// vector<string> s_vec;
 
 int countRead(int num){
     int cnt = 0;
@@ -11,6 +12,21 @@ int countRead(int num){
     }
     return cnt;
 }
+
+/*
+int countRead(int num){
+    int cnt = 0;
+    for(string s : s_vec){
+        bool flag = true;
+        for(int i = 4; i < s.length() - 4; i++){
+            int _temp = (1 << (int)(s[i] - 'a'));
+            if((num & _temp) == 0) { flag = false; break; } 
+        } // s[i]자리의 알바벳이 num의 비트자리가 켜져있으면 통과
+        if(flag) cnt++;
+    }
+    return cnt;
+}
+*/
 
 void DFS(int idx, int num, int cnt){
     if(cnt == K){
@@ -41,18 +57,21 @@ int main(){
     learn |= (1 << (int)('c'-'a'));
 
     cin >> N >> K;
-
     for(int i = 0; i < N; i++){
         string s; cin >> s;
         for(char c : s){
             words[i] |= (1 << (int)(c - 'a'));
         }
     }
-
+    /*
+    for(int i = 0; i < N; i++){
+        string s; cin >> s;
+        s_vec.push_back(s);
+    }
+    */
     if(K < 5){ cout << "0\n"; return 0; }
 
     DFS(0, learn, 5);
-
     cout << ret << "\n";
     return 0;
 }
