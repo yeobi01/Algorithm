9e38ba313b655f1691ea85927c912055fbf4e8f5
yeobi01 <yeobi_01@naver.com>
2023-01-10 00:12:37 +0900

    C++

--- 큰돌_C++/4주차_비트마스킹/4I_11723.cpp
+++ 큰돌_C++/4주차_비트마스킹/4I_11723.cpp
@@ -4,12 +4,35 @@ const int INF = 987654321;
 const int dy[4] = {0, -1, 0, 1};
 const int dx[4] = {-1, 0, 1, 0};
 
-int N, M, cnt, mx, big, arr[51][51], visited[51][51], comSize[2501];
-
+int N, temp, num;
+string s;
 
 int main(){
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
+    cin >> N;
+    for(int i = 0; i < N; i++){
+        cin >> s;
+        if(s == "add"){
+            cin >> temp;
+            num |= (1 << temp);
+        } else if(s == "remove"){
+            cin >> temp;
+            num &= ~(1 << temp);
+        } else if(s == "check"){
+            cin >> temp;
+            if(num & (1 << temp)) cout << "1\n";
+            else cout << "0\n";
+        } else if(s == "toggle"){
+            cin >> temp;
+            if(num & (1 << temp)) num &= ~(1 << temp);
+            else num |= (1 << temp);
+        } else if(s == "all"){
+            num = pow(2, 21) - 1;
+        } else if(s == "empty"){
+            num = 0;
+        }
+    }
     return 0;
 }
---
+++ 큰돌_C++/4주차_비트마스킹/4J_14391.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;   
+const int INF = 987654321;
+const int dy[4] = {0, -1, 0, 1};
+const int dx[4] = {-1, 0, 1, 0};
+
+int N, M, arr[4][4], ret;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+    cin >> N >> M;
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < M; j++){
+            char c; cin >> c;
+            arr[i][j] = c - '0';
+        }
+    }
+
+    for(int t = 0; t < (1 << N*M); t++){
+        int sum = 0;
+        for(int i = 0; i < N; i++){
+            int temp = 0;
+            for(int j = 0; j < M; j++){
+                int idx = i * M + j;
+                if((t & (1 << idx)) == 0) temp = temp * 10 + arr[i][j];
+                else { sum += temp; temp = 0; }
+            }
+            if(temp != 0) sum += temp;
+        }
+        for(int j = 0; j < M; j++){
+            int temp = 0;
+            for(int i = 0; i < N; i++){
+                int idx = i * M + j;
+                if(t & (1 << idx)) temp = temp * 10 + arr[i][j];
+                else { sum += temp; temp = 0; }
+            }
+            if(temp != 0) sum += temp;
+        }
+        ret = max(ret, sum);
+    }
+    cout << ret << "\n";
+    return 0;
+}
