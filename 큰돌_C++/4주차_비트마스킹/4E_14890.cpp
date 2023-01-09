c438123b7b0f68be104b884b64054490f84408a4
yeobi01 <yeobi_01@naver.com>
2023-01-09 21:49:09 +0900

    C++

--- 큰돌_C++/4주차_비트마스킹/4E_14890.cpp
+++ 큰돌_C++/4주차_비트마스킹/4E_14890.cpp
@@ -4,8 +4,22 @@ const int INF = 987654321;
 const int dy[4] = {0, 0, -1, 1};
 const int dx[4] = {1, -1, 0, 0};
 
-int N, L, arr[101][101], can_R, can_C;
+int N, L, a[104][104], b[104][104], ret;
 
+void solve(int arr[104][104]){
+	for(int i = 0; i < N; i++){
+		int cnt = 1;
+		int j;
+		for(j = 0; j < N - 1; j++){
+			if(arr[i][j] == arr[i][j + 1]) cnt++;
+			else if((arr[i][j] + 1 == arr[i][j + 1]) && cnt >= L) cnt = 1;
+			else if((arr[i][j] - 1 == arr[i][j + 1]) && cnt >= 0) cnt = 1 - L;
+			else break;
+		}
+		if(j == N - 1 && cnt >= 0) ret++;
+	}
+	return;
+}
 int main(){
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
@@ -13,11 +27,11 @@ int main(){
 	cin >> N >> L;
 	for(int i = 0; i < N; i++){
 		for(int j = 0; j < N; j++){
-			cin >> arr[i][j];
+			cin >> a[i][j];
+			b[j][i] = a[i][j];
 		}
 	}
-
-	cout << "0\n";
-	
+	solve(a); solve(b);
+	cout << ret << "\n";
     return 0;
 }
