81b19b4fdac863b68b60d9f333b3fe604eb364f8
yeobi01 <yeobi_01@naver.com>
2023-01-10 18:49:00 +0900

    C++

--- 큰돌_C++/4주차_비트마스킹/4M_14405.cpp
+++ 큰돌_C++/4주차_비트마스킹/4M_14405.cpp
@@ -4,12 +4,23 @@ const int INF = 987654321;
 const int dy[4] = {0, -1, 0, 1};
 const int dx[4] = {-1, 0, 1, 0};
 
-int T;
+string s;
+bool flag = true;
 
 int main(){
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
+    cin >> s; 
+    for(int i = 0; i < s.size(); ){
+        if(s.substr(i, 2) == "pi") i += 2;
+        else if(s.substr(i, 2) == "ka") i += 2;
+        else if(s.substr(i, 3) == "chu") i += 3;
+        else { flag = false; break; }
+    }
+
+    if(flag) cout << "YES\n";
+    else cout << "NO\n";
 
     return 0;
 }
