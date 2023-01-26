cd235e9a65947a74c7ab6e5b5a7476eea7d2b7c4
yeobi01 <yeobi_01@naver.com>
2023-01-26 15:50:50 +0900

    C++

--- 큰돌_C++/5주차_그리디_라인스위핑_투포인터/5I_3273.cpp
+++ 큰돌_C++/5주차_그리디_라인스위핑_투포인터/5I_3273.cpp
@@ -2,14 +2,26 @@
 using namespace std;
 const int INF = 987654321;
 
-long long s, e, N, cnt[100001], arr[100001];
-long long ret;
+int N, s, e, arr[100001], x, ret;
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
+    cin >> N;
+    for(int i = 0; i < N; i++) cin >> arr[i];
+    cin >> x;
+    sort(arr, arr + N);
 
+    e = N - 1;
+    while(s < e){
+        int temp = arr[s] + arr[e];
+        if(temp > x) e--;
+        else if(temp < x) s++;
+        else{ ret++; s++; e--; }
+    }
+
+    cout << ret << "\n";
     return 0;
 }
