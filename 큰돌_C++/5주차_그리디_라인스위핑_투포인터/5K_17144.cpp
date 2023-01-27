2a6497b9126c62ff207304f68ae774f408c37055
yeobi01 <yeobi_01@naver.com>
2023-01-27 16:17:33 +0900

    C++

--- 큰돌_C++/5주차_그리디_라인스위핑_투포인터/5K_17144.cpp
+++ 큰돌_C++/5주차_그리디_라인스위핑_투포인터/5K_17144.cpp
@@ -23,30 +23,60 @@ int main(){
         }
     }
 
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < M; j++){
-            if(arr[i][j] > 0) v.push_back({arr[i][j], {i, j}});
+    while(T--){
+        v.clear();
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < M; j++){
+                if(arr[i][j] > 0) v.push_back({arr[i][j], {i, j}});
+            }
         }
-    }
-    for(auto temp : v){
-        int amount = temp.first;
-        int y = temp.second.first;
-        int x = temp.second.second;
-        for(int i = 0; i < 4; i++){
-            int ny = y + dy[i];
-            int nx = x + dx[i];
-            if(ny < 0 || ny >= N || nx < 0 || nx >= M) continue;
-            if(nx == 0 && (ny == air_y1 || ny == air_y2)) continue;
-            arr[ny][nx] += amount/5;
-            arr[y][x] -= amount/5;
+        for(auto temp : v){
+            int amount = temp.first;
+            int y = temp.second.first;
+            int x = temp.second.second;
+            for(int i = 0; i < 4; i++){
+                int ny = y + dy[i];
+                int nx = x + dx[i];
+                if(ny < 0 || ny >= N || nx < 0 || nx >= M) continue;
+                if(nx == 0 && (ny == air_y1 || ny == air_y2)) continue;
+                arr[ny][nx] += amount/5;
+                arr[y][x] -= amount/5;
+            }
         }
-    }
-    cout << "\n";
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < M; j++){
-            cout << arr[i][j] << " ";
-        } cout << "\n";
+
+        for(int i = air_y1 - 1; i > 0; i--){
+            arr[i][0] = arr[i - 1][0];
+        }
+        for(int i = 0; i < M - 1; i++){
+            arr[0][i] = arr[0][i + 1];
+        }
+        for(int i = 0; i < air_y1; i++){
+            arr[i][M - 1] = arr[i + 1][M - 1];
+        }
+        for(int i = M - 1; i > 1; i--){
+            arr[air_y1][i] = arr[air_y1][i - 1];
+        } arr[air_y1][1] = 0;
+
+        for(int i = air_y2 + 1; i < N - 1; i++){
+            arr[i][0] = arr[i + 1][0];
+        }
+        for(int i = 0; i < M - 1; i++){
+            arr[N - 1][i] = arr[N - 1][i + 1];
+        }
+        for(int i = N - 1; i > air_y2; i--){
+            arr[i][M - 1] = arr[i - 1][M - 1];
+        }
+        for(int i = M - 1; i > 1; i--){
+            arr[air_y2][i] = arr[air_y2][i - 1];
+        } arr[air_y2][1] = 0;
     }
 
+    int ret = 0;
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < M; j++){
+            if(arr[i][j] > 0) ret += arr[i][j];
+        }
+    }
+    cout << ret << "\n";
     return 0;
 }
