7fe981c45807346cdaf023d0402517c92cdbe3c2
yeobi01 <yeobi_01@naver.com>
2023-02-16 20:40:38 +0900

    C++

--- 큰돌_C++/5주차_그리디_라인스위핑_투포인터/5Z_2170.cpp
+++ 큰돌_C++/5주차_그리디_라인스위핑_투포인터/5Z_2170.cpp
@@ -5,19 +5,22 @@ const int dy[4] = {0, -1, 0, 1};
 const int dx[4] = {1, 0, -1, 0};
 
 pair<int, int> L[1000004];
-int n, from, to, l, r, ret; 
+int N, ret; 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    cout.tie(NULL); 
-    cin >> n; 
-    for(int i = 0; i < n; i++){ 
-        cin >> from >> to;
-        L[i] = {from, to};
+    cout.tie(NULL);
+
+    cin >> N; 
+    for(int i = 0; i < N; i++){ 
+        int a, b; cin >> a >> b;
+        L[i] = {a, b};
     }
-    sort(L, L + n); 
-    l = L[0].first; r = L[0].second; 
-    for(int i = 1; i < n; i++){ 
+    sort(L, L + N); 
+    
+    int l = L[0].first; 
+    int r = L[0].second; 
+    for(int i = 1; i < N; i++){ 
         if(r < L[i].first){ 
             ret += (r - l); 
             l = L[i].first;
@@ -27,5 +30,7 @@ int main(){
         }
     }
     ret += r - l;
-    cout << ret << '\n';
+
+    cout << ret << "\n";
+    return 0;
 }
