df8f74533f92f82a8b9c92ab4986e2eb30d85740
yeobi01 <yeobi_01@naver.com>
2023-02-18 22:21:30 +0900

    C++

---
+++ 큰돌_C++/6주차_이분탐색_LIS/6C_6236.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int INF = 987654321;
+
+int N, M, arr[100001];
+
+bool check(int mid){
+    int cnt = 1;
+    int real_mid = mid;
+    for(int i = 0; i < N; i++){
+        if(mid - arr[i] >= 0){
+            mid -= arr[i];
+        } else{
+            mid = real_mid;
+            cnt++;
+            if(arr[i] > mid) return false;
+            else mid -= arr[i];
+        }
+    }
+    return cnt <= M;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> N >> M;
+    for(int i = 0; i < N; i++){
+        cin >> arr[i];
+    }
+
+    ll low = 1, high = 1000000004, ret;
+    while(low <= high){
+        int mid = (low + high) / 2;
+        if(check(mid)){
+            high = mid - 1;
+            ret = mid;
+        } else low = mid + 1;
+    }
+    cout << ret << "\n";
+    return 0;
+}
