fc5abedc159d40e70c63f570316fabb05020e58f
yeobi01 <yeobi_01@naver.com>
2023-02-19 14:58:07 +0900

    C++

---
+++ 큰돌_C++/6주차_이분탐색_LIS/6D_7795.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int INF = 987654321;
+
+int T, N, M;
+vector<int> a, b;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> T;
+    while(T--){
+        int ret = 0, temp;
+        a.clear(); b.clear();
+
+        cin >> N >> M;
+        for(int i = 0; i < N; i++) cin >> temp, a.push_back(temp);
+        for(int i = 0; i < M; i++) cin >> temp, b.push_back(temp);
+        sort(a.begin(), a.end());
+        sort(b.begin(), b.end());
+        
+        for(int x : a){
+            if(x <= b[0]) continue;
+
+            int low = 0, high = M - 1, mid, temp;
+            while(low <= high){
+                mid = (low + high) / 2;
+                if(x > b[mid]){
+                    temp = mid;
+                    low = mid + 1;
+                } else{
+                    high = mid - 1;
+                }
+            }
+            ret += (temp + 1);
+        }
+        cout << ret << "\n";
+    }
+    return 0;
+}
