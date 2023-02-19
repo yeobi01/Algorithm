bbb72e7a1cb3795043fde9c88a1f10098d0a8e1d
yeobi01 <yeobi_01@naver.com>
2023-02-19 16:07:11 +0900

    C++

---
+++ 큰돌_C++/6주차_이분탐색_LIS/6G_1072.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const ll INF = 1e9;
+
+ll X, Y, Z;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> X >> Y;
+    Z = (100 * Y) / X;
+
+    ll low = 0, high = INF, mid, ret = -1;
+    while(low <= high){
+        mid = (low + high) / 2;
+        ll nZ = (100 * (Y + mid)) / (X + mid);
+        if(Z != nZ){
+            ret = mid;
+            high = mid - 1;
+        } else{
+            low = mid + 1;
+        }
+    }
+    cout << ret << "\n";
+    return 0;
+}
