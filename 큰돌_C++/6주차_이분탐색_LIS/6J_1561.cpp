b9609c0c22f0de88199fca453329602b8751170b
yeobi01 <yeobi_01@naver.com>
2023-02-20 17:25:12 +0900

    C++

---
+++ 큰돌_C++/6주차_이분탐색_LIS/6J_1561.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int INF = 1e9;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    return 0;
+}
