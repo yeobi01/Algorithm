76f5c68e81dde3bc921d188eeb8643f89caed311
yeobi01 <yeobi_01@naver.com>
2023-02-24 22:59:46 +0900

    C++

--- 큰돌_C++/6주차_이분탐색_LIS/6M_11053.cpp
+++ 큰돌_C++/6주차_이분탐색_LIS/6M_11053.cpp
@@ -3,9 +3,25 @@ using namespace std;
 typedef long long ll;
 const int INF = 987654321;
 
-int N, temp, lis[1004], len;
+int N, temp;
 vector<int> v;
 
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> N;
+    for(int i = 0; i < N; i++){
+        cin >> temp;
+        int lowerIdx = (lower_bound(v.begin(), v.end(), temp) - v.begin());
+        if(v.size() == lowerIdx) v.push_back(temp);
+        else v[lowerIdx] = temp;
+    }
+    cout << v.size() << "\n";
+    return 0;
+}
+/*
+int N, temp, lis[1004], len;
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -15,9 +31,11 @@ int main(){
     for(int i = 0; i < N; i++){
         cin >> temp;
         int lowerIdx = lower_bound(lis, lis + len, temp) - lis;
+        cout << i << " " << len << " " << lowerIdx << "\n";
         if(lis[lowerIdx] == 0) len++;
         lis[lowerIdx] = temp;
     }
     cout << len << "\n";
     return 0;
 }
+*/
