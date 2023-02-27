bd8951b87dc4a024000bb4bb7245e85aa70c3d07
yeobi01 <yeobi_01@naver.com>
2023-02-27 10:33:48 +0900

    C++

--- 2023_NPC_신입관문/4번_27562.cpp
+++ 2023_NPC_신입관문/4번_27562.cpp
@@ -9,6 +9,60 @@ struct Aircon{
     int a, b, p, m;
 };
 
+int N, M, ret = INF;
+Cow cow[21];
+Aircon air[11];
+
+void check(int visited){
+    int sum[101] = {0, }, temp_money = 0;
+    for(int i = 0; i < M; i++){
+        if(visited & (1 << i)){
+            temp_money += air[i].m;
+            for(int j = air[i].a; j <= air[i].b; j++){
+                sum[j] += air[i].p;
+            } 
+        }
+    }
+    bool flag = true;
+    for(int i = 0; i < N && flag; i++){
+        for(int j = cow[i].s; j <= cow[i].t && flag; j++){
+            if(sum[j] < cow[i].c) flag = false;
+        }
+    }
+    if(flag) ret = min(ret, temp_money);
+    return;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N >> M;
+    for(int i = 0; i < N; i++){
+        int s, t, c; cin >> s >> t >> c;
+        cow[i] = {s, t, c};
+    }
+    for(int i = 0; i < M; i++){
+        int a, b, p, m; cin >> a >> b >> p >> m;
+        air[i] = {a, b, p, m};
+    }
+
+    for(int i = 0; i < pow(2, M); i++) check(i);
+
+    cout << ret << "\n"; 
+
+    return 0;
+}
+
+/*
+struct Cow{
+    int s, t, c;
+};
+struct Aircon{
+    int a, b, p, m;
+};
+
 int N, M, ret = INF, visited[11];
 Cow cow[21];
 Aircon air[11];
@@ -64,4 +118,4 @@ int main(){
     cout << ret << "\n"; 
 
     return 0;
-}
+}*/
