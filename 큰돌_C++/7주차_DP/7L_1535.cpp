33bedf9889e5d71e6a134f1c2db99ec13f2196d4
yeobi01 <yeobi_01@naver.com>
2023-03-03 22:58:14 +0900

    C++

--- 큰돌_C++/7주차_DP/7L_1535.cpp
+++ 큰돌_C++/7주차_DP/7L_1535.cpp
@@ -2,12 +2,25 @@
 using namespace std;
 const int INF = 987654321;
 
+int N, L[21], J[21], dp[101];
+vector<int> v;
+
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
+    cin >> N;
+    for(int i = 0; i < N; i++) cin >> L[i];
+    for(int i = 0; i < N; i++) cin >> J[i];
 
+    for(int i = 0; i < N; i++){
+        for(int j = 100; j > L[i]; j--){
+            dp[j] = max(dp[j], dp[j - L[i]] + J[i]);
+        }
+    }
+
+    cout << dp[100] << "\n";
 
     return 0;
 }
