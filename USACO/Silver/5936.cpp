674dfeacc35f15b64ce364c28df344a8ec5ef9da
yeobi01 <yeobi_01@naver.com>
2023-04-06 19:55:26 +0900

    C++

--- USACO/Silver/5936.cpp
+++ USACO/Silver/5936.cpp
@@ -3,12 +3,12 @@ using namespace std;
 typedef long long ll;
 const int INF = 987654321;
 
-ll N, Nopt, F, M, ret, memo[200001];
-bool FN[40001], flag = false;;
+ll N, Nopt, F, M, ret, memo[200001], min_C = INF;
+bool FN[40001], flag = false;
 vector<ll> C;
 
 ll go(ll cnt, ll eat){
-    if(cnt == 0){
+    if(cnt < min_C){
         return eat;
     }
 
@@ -22,11 +22,11 @@ ll go(ll cnt, ll eat){
     ans = 0;
     for(ll C_i : C){
         if(cnt - C_i >= 0){
-            if(FN[cnt - C_i]){
-                ans = max(ans, go(cnt - C_i + M, eat + C_i));
-            } else{
-                ans = max(ans, go(cnt - C_i, eat + C_i));
+            ll temp = cnt - C_i;
+            while(FN[temp]){
+                temp += M;
             }
+            ans = max(ans, go(temp, eat + C_i));
         }
     }
 
@@ -37,6 +37,7 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
+    // cin.tie(0)->sync_with_stdio(0);
 
     memset(memo, -1, sizeof(memo));
 
@@ -44,12 +45,12 @@ int main(){
     for(int i = 0; i < Nopt; i++){
         ll temp; cin >> temp;
         C.push_back(temp);
+        min_C = min(min_C, temp);
     }
     for(int i = 0; i < F; i++){
         ll temp; cin >> temp;
         FN[temp] = true;
     }
-
     ret = go(N, 0);
 
     cout << ret << "\n";
@@ -57,6 +58,18 @@ int main(){
     return 0;
 }
 
+
+/*
+10 1 2 1
+2 
+8
+9
+*/
+/*
+if(cnt == 0){
+    return eat;
+}
+*/
 /*
 if(cnt == N && flag){
     cout << "-1\n";
