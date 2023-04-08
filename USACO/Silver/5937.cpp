5b02dd0ffac90c1ee9931128f9d364928f7ddc1c
yeobi01 <yeobi_01@naver.com>
2023-04-08 22:35:18 +0900

    C++

---
+++ USACO/Silver/5937.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;   
+typedef long long ll;
+const int INF = 987654321;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    // cin.tie(0)->sync_with_stdio(0);
+
+
+
+    return 0;
+}
