8bac03b208a0e8f539998b3a5c64851dd0383548
yeobi01 <yeobi_01@naver.com>
2023-04-14 22:48:22 +0900

    C++

--- USACO/Silver/5937.cpp
+++ USACO/Silver/5937.cpp
@@ -4,9 +4,9 @@ typedef long long ll;
 const int INF = 987654321;
 
 int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    // cin.tie(0)->sync_with_stdio(0);
+    cin.tie(0)->sync_with_stdio(0);
+    
+    
+    
     return 0;
 }
