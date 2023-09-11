87fc805c9c6e4e81c3863ac1160212c2d8c34786
Kim Sang Yeob <73064401+yeobi01@users.noreply.github.com>
2023-09-11 18:54:47 +0900

    [Silver II] Title: 증가하는 부분 수열의 개수, Time: 44 ms, Memory: 2100 KB -BaekjoonHub

---
+++ 백준/Silver/22971. 증가하는 부분 수열의 개수/증가하는 부분 수열의 개수.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+ll N;
+ll arr[5001];
+ll ans[5001];
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N;
+    for(int i = 0; i < N; i++){
+        cin >> arr[i];
+    }
+    ans[0] = 1;
+    for(int i = 1; i < N; i++){
+        for(int j = 0; j < i; j++){
+            if(arr[i] > arr[j]){
+                ans[i] += ans[j];
+                ans[i] %= 998244353;
+            }
+        }
+        ans[i]++;
+        ans[i] %= 998244353;
+    }
+    for(int i = 0; i < N; i++){
+        cout << ans[i] << " ";
+    }
+    cout << "\n";
+
+    return 0;
+}
