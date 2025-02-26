4c7752d3b1ae92ff6c87813c40826d7fa8b06902
Kim Sang Yeob <73064401+yeobi01@users.noreply.github.com>
2025-02-26 15:47:51 +0900

    [Silver I] Title: 볼 모으기, Time: 8 ms, Memory: 2508 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/17615. 볼 모으기/볼 모으기.cc
+++ 백준/Silver/17615. 볼 모으기/볼 모으기.cc
@@ -4,7 +4,7 @@
 using namespace std;
 typedef long long ll;
 
-int n, R_count, B_count;
+int n, Rr_count, Br_count, Rl_count, Bl_count;
 char RB[500000];
 
 int main(){
@@ -13,18 +13,28 @@ int main(){
     cin >> n;
     for(int i = n - 1; i >= 0; i--){
         cin >> RB[i];
-        if(RB[i] == 'R') R_count++;
-        if(RB[i] == 'B') B_count++;
+        if(RB[i] == 'R') Rr_count++;
+        if(RB[i] == 'B') Br_count++;
     }
+    Rl_count = Rr_count;
+    Bl_count = Br_count;
     
     char start_color = RB[0];
     for(int i = 0; i < n; i++){
         if(RB[i] != start_color) break;
-        if(RB[i] == start_color && RB[i] == 'R') R_count--;
-        if(RB[i] == start_color && RB[i] == 'B') B_count--;
+        if(RB[i] == start_color && RB[i] == 'R') Rr_count--;
+        if(RB[i] == start_color && RB[i] == 'B') Br_count--;
+    }
+    char end_color = RB[n - 1];
+    for(int i = n - 1; i >= 0; i--){
+        if(RB[i] != end_color) break;
+        if(RB[i] == end_color && RB[i] == 'R') Rl_count--;
+        if(RB[i] == end_color && RB[i] == 'B') Bl_count--;
     }
 
-    int res = R_count < B_count ? R_count : B_count;
+    int r_min = Rr_count < Br_count ? Rr_count : Br_count;
+    int l_min = Rl_count < Bl_count ? Rl_count : Bl_count;
+    int res = r_min < l_min ? r_min : l_min;
     cout << res << "\n";
 
     return 0;
