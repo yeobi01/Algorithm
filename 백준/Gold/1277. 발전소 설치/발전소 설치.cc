0b62d0f8c47e0c4dadd65e19affb85e8ed05cb66
Kim Sang Yeob <73064401+yeobi01@users.noreply.github.com>
2025-03-06 00:32:57 +0900

    [Gold IV] Title: 발전소 설치, Time: 8 ms, Memory: 9988 KB -BaekjoonHub

--- 백준/Gold/1277. 발전소 설치/발전소 설치.cc
+++ 백준/Gold/1277. 발전소 설치/발전소 설치.cc
@@ -1,64 +1,85 @@
-#include "bits/stdc++.h"
+// 1277번. 발전소 설치
+
+#include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 
-struct Node {
-    int n;
-    double cost;
-};
-struct compare {
-    bool operator()(const Node l,const Node r) {
-        return l.cost>r.cost;
+#define INF 987654321;
+
+int N, W;
+vector<pair<int, int>> v;
+double connect[1000][1000];
+double dist[1000];
+bool visited[1000];
+double line_length_max;
+
+int get_min_index(){
+    double _min = INF;
+    int _min_idx = 0;
+    for(int i = 0; i < N; i++){
+        if(visited[i]) continue;
+        if(_min > dist[i]){
+            _min = dist[i];
+            _min_idx = i;
+        }
     }
-};
+    // if(_min_idx != -1) visited[_min_idx] = true;
+    return _min_idx;
+}
 
-#define MX 5000000000
+int main(){
+    cin.tie(0)->sync_with_stdio(0);
 
-int main() {
-    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    int N, W; cin>>N>>W;
-    double M; cin>>M;
-    int locx[N+1];
-    int locy[N+1];
-    double nodes[N+1][N+1];
-    double dp[N+1];
-    for (int i = 1 ; i<=N; ++i) {
-        int x,y; cin>>x>>y;
-        locx[i] = x; locy[i] = y;
+    cin >> N >> W;
+    cin >> line_length_max;
+    for(int i = 0; i < N; i++){
+        int x; int y; cin >> x >> y;
+        v.push_back({x, y});
     }
-    for(int i=1; i<=N; ++i) {
-        dp[i] = MX;
-        for(int j=1;j<=N;++j) {
-            double x = (locx[i]-locx[j]);
-            double y = (locy[i]-locy[j]);
-            double dist = sqrt(x*x+y*y);
-            if(dist<=M) {
-                nodes[i][j] = dist;
-            } else {
-                nodes[i][j] = MX;
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            double x = v[i].first - v[j].first;
+            double y = v[i].second - v[j].second;
+            double length = sqrt(x*x + y*y);
+            if(length <= line_length_max){
+                connect[i][j] = length;
+            } else{
+                connect[i][j] = INF;
             }
         }
     }
     
-    for(int i=0;i<W;++i) {
-        int x,y; cin>>x>>y;
-        nodes[x][y]=0; nodes[y][x]=0;
+    for(int i = 0; i < W; i++){
+        int a; int b; cin >> a >> b;
+        connect[a-1][b-1] = 0;
+        connect[b-1][a-1] = 0;
     }
-    priority_queue<Node,vector<Node>,compare> que;
-    que.push({1,0.0});
-    dp[1] = 0;
-    
-    while(!que.empty()) {
-        Node f = que.top(); que.pop();
-        if(dp[f.n] < f.cost) { continue; }
-        if(f.n == N) { break; }
-        for (int i =1; i<=N; ++i) {
-            if(nodes[f.n][i] == MX) { continue;}
-            double nextCost = f.cost + nodes[f.n][i];
-            if(dp[i] > nextCost ) {
-                dp[i] = nextCost;
-                que.push({i,nextCost});
+
+    // for(int i = 0; i < N; i++){
+    //     for(int j = 0; j < N; j++){
+    //         cout << connect[i][j] << " ";
+    //     } cout << "\n";
+    // }
+
+    for(int i = 0; i < N; i++) dist[i] = connect[0][i];
+    visited[0] = true;
+    for(int i = 0; i < N - 2; i++){
+        int cur = get_min_index();
+        visited[cur] = true;
+        
+        // if(i == -1) break;
+        for(int j = 0; j < N; j++){
+            if(visited[j]) continue;
+            if(dist[j] > dist[cur] + connect[cur][j]){
+                dist[j] = dist[cur] + connect[cur][j];
             }
         }
     }
-    cout<<(long long)(floor(dp[N]*1000));
+    // for(int i = 0; i < N; i++){
+    //     cout << dist[i] << " ";
+    // }
+    int res = dist[N - 1] * 1000;
+    cout << res << "\n";
+
+    return 0;
 }
