fd88924234aa92a4f933054d7852ae5aa23498f7
Kim Sang Yeob <yeobi_01@naver.com>
2025-10-17 18:18:55 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,6 +1,4 @@
 def solution(brown, yellow):
-    answer = []
-    
     x = (int)(((brown / 2 + 2) + ((brown / 2 + 2) ** 2 - 4 * (brown + yellow)) ** 0.5) / 2)
     y =  (int)(((brown / 2 + 2) - ((brown / 2 + 2) ** 2 - 4 * (brown + yellow)) ** 0.5) / 2)   
     
